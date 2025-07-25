aaef6941dae57a5ebef495c020db42a8a38bffb8
Abassgold <Abasskola10@gmail.com>
2025-07-25 10:50:28 +0100

    jshjhsjhs

--- src/app/(user)/login/api/route.ts
+++ src/app/(user)/login/api/route.ts
@@ -1,6 +1,8 @@
 import { findUser } from "@/redux/type";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
+    const cookieStore = await cookies()
     const body = await req.json();
 
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/login`, {
@@ -10,10 +12,10 @@ export async function POST(req: NextRequest) {
     });
 
     const data: findUser = await res.json();
-    if (!res.ok) return NextResponse.json({
+    if(!res.ok) return NextResponse.json({
         ok: data.ok,
         msg: data.msg
-    }, { status: res.status });
+    },{status: res.status});
 
     if (!data.ok) {
         return NextResponse.json({
@@ -24,7 +26,12 @@ export async function POST(req: NextRequest) {
             { status: 200 }
         )
     }
-
+    cookieStore.set('accessToken', data.token || '', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: 60 * 60 * 24 * 30
+    });
     return NextResponse.json(
         {
             ok: data.ok,
@@ -32,12 +39,5 @@ export async function POST(req: NextRequest) {
             user: data.user,
         },
         { status: 200 }
-    ).cookies.set({
-        name: "accessToken",
-        value: data.token || "",
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'none',
-        maxAge: 60 * 60 * 24 * 30,
-    });
+    );
 }
