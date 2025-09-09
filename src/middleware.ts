b0685b71ea7ea8136b182ee94d4332df0c89b5bd
Abassgold <Abasskola10@gmail.com>
2025-09-09 01:30:59 +0100

    hjjhk'

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,29 +3,29 @@ import type { NextRequest } from 'next/server'
 import { isAdmin, isAuthenticated } from './middlewares/api/authMiddleware';
 
 export async function middleware(req: NextRequest) {
-    // const token = req.cookies.get('accessToken')?.value || '';
-    // const adminRoutes = ['/admin'];
+    const token = req.cookies.get('accessToken')?.value || '';
+    const adminRoutes = ['/admin'];
 
-    // const authStatus = await isAuthenticated(token);
+    const authStatus = await isAuthenticated(token);
 
-    // if (!token || !authStatus.authenticated) {
-    //     return NextResponse.redirect(new URL('/login', req.url));
-    // }
-    // if (authStatus.suspended) {
-    //     return NextResponse.redirect(new URL('/account-suspended', req.url));
-    // }
-    // if (adminRoutes.some(route => req.nextUrl.pathname.startsWith(route))) {
-    //     if (!(await isAdmin(token))) {
-    //         return NextResponse.redirect(new URL('/login', req.url));
-    //     }
-    // }
-    // return NextResponse.next();
+    if (!token || !authStatus.authenticated) {
+        return NextResponse.redirect(new URL('/login', req.url));
+    }
+    if (authStatus.suspended) {
+        return NextResponse.redirect(new URL('/account-suspended', req.url));
+    }
+    if (adminRoutes.some(route => req.nextUrl.pathname.startsWith(route))) {
+        if (!(await isAdmin(token))) {
+            return NextResponse.redirect(new URL('/login', req.url));
+        }
+    }
+    return NextResponse.next();
 }
 
 
-// export const config = {
-//     matcher: [
-//         '/user/:path*',
-//         '/admin/:path*'
-//     ],
-// };
+export const config = {
+    matcher: [
+        '/user/:path*',
+        '/admin/:path*'
+    ],
+};
