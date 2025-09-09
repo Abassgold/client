5923a453409eab4d14c91db78e4aad731f60c898
Abassgold <Abasskola10@gmail.com>
2025-09-09 01:30:28 +0100

    hjjhk'

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col md:flex-row h-screen">
-      <aside>
-        <AdminLayout />
-      </aside>
-      <main className="flex-1 overflow-y-auto p-4">
+      <div className="flex flex-col md:flex-row h-screen">
+        <aside>
+          <AdminLayout />
+        </aside>
+        <main className="flex-1 overflow-y-auto p-4">
         {children}
       </main>
     </div>
--- src/app/(dasboard)/layout.tsx
+++ src/app/(dasboard)/layout.tsx
@@ -3,17 +3,17 @@ import { ResponsiveSidebar } from "@/components/dashboard/DashboardAside";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex  flex-col md:flex-row h-screen relative">
-      <ResponsiveSidebar />
-      <div className="flex-1 flex flex-col">
-        <div className="sticky top-0 z-20 w-full">
-          <OverviwHeader />
-        </div>
+      <div className="flex  flex-col md:flex-row h-screen relative">
+        <ResponsiveSidebar />
+        <div className="flex-1 flex flex-col">
+          <div className="sticky top-0 z-20 w-full">
+            <OverviwHeader />
+          </div>
 
-        <main className="flex-1 overflow-y-auto p-2 md:p-4 bg-gray-100">
-          {children}
-        </main>
+          <main className="flex-1 overflow-y-auto p-2 md:p-4 bg-gray-100">
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
   );
 }
