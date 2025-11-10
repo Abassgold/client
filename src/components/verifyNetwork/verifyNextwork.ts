7d680ca8e71d25f19171763aec88435b286055ee
Abassgold <Abasskola10@gmail.com>
2025-11-10 07:35:08 +0100

    write regex for network checker

--- src/components/verifyNetwork/verifyNextwork.ts
+++ src/components/verifyNetwork/verifyNextwork.ts
@@ -2,9 +2,9 @@ export function getNetworkByRegex(phoneNumber: string) {
   phoneNumber = phoneNumber.replace('+234', '0').trim();
 if(phoneNumber.length < 11 || phoneNumber.length > 14 ) return 'UNKNOWN'
   const patterns = {
-    MTN: /^(0803|0806|0703|0706|0810|0813|0814|0816|0903|0906|0913|0916)/,
+    MTN: /^(0803|0806|0703|0706|0810|0813|0814|0816|0903|0906|0913|0916|07025|07026|0704|0707)/,
     GLO: /^(0805|0807|0705|0811|0815|0905|0915)/,
-    AIRTEL: /^(0802|0808|0708|0812|0701|0902|0907|0901|0912|0911)/,
+    AIRTEL: /^(0802|0808|0708|0812|0701|0902|0907|0901|0912|0911|0904)/,
     '9MOBILE': /^(0809|0817|0818|0909|0908)/
   } as const;
 
